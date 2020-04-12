5312f6303d7a929b8175b27f29b5b41bd87774bc
benjamiam <bblackmon@gmail.com>
2020-04-12 00:07:33 -0500

    added array info

--- sweepstakes.js
+++ sweepstakes.js
@@ -5,23 +5,21 @@
 const puppeteer = require('puppeteer');
 var dt = new Date();
 
-import { info } from './infobb.js';
-import { local_info } from './infobb.js';
-
+const data = require('./infobb.js');
 
 (async () => {
 
 	const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 	const page = await browser.newPage();
-	await page.goto(local_info.website);
+	await page.goto(data.local_info[0]);
 
-	await page.type('#first_name', info.fname)
-	await page.type('#last_name', info.lname)
-	await page.type('#email', info.email)
-	await page.type('#street', info.address)
-	await page.type('#city', info.city)
-	await page.type('#state', info.state)
-	await page.type('#zip_code', info.zip)
+	await page.type('#first_name', data.info[0])
+	await page.type('#last_name', data.info[1])
+	await page.type('#email', data.info[2])
+	await page.type('#street', data.info[3])
+	await page.type('#city', data.info[4])
+	await page.type('#state', data.info[5])
+	await page.type('#zip_code', data.info[6])
 
 	await page.$$eval('input[type="checkbox"]', checkboxes=> {
 		checkboxes.forEach(chkbox => chkbox.click())
@@ -32,20 +30,20 @@ import { local_info } from './infobb.js';
 		page.click('#send2')
 	]);
 */
-	await page.screenshot({path: local_info.screenshot, fullPage: true});
+	await page.screenshot({path: data.local_info[1], fullPage: true});
 
 	await browser.close();
 
 	await console.log(dt.toString() + info.fname + " Success");
 
 })();
-
+/*
 	var fs = require('fs')
-	var logger = fs.createWriteStream(local_info.log, {
+	var logger = fs.createWriteStream(data.local_info[3], {
 		flags: 'a' // append 
 	})
-	logger.write("\r\n"+ dt.toString() + info.fname + "  Success");
-
+	logger.write("\r\n"+ dt.toString() + data.info.fname + "  Success");
+*/
 /*
 }
 
