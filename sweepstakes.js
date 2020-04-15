d1684399ba4af1ea87153eb6f349047889b15cd4
benjamiam <bblackmon@gmail.com>
2020-04-15 02:08:24 -0500

    Neat & Tidy

--- sweepstakes.js
+++ sweepstakes.js
@@ -1,6 +1,6 @@
 
 
-function intervalFunc() {
+// function intervalFunc() {
 
 const puppeteer = require('puppeteer');
 var dt = new Date();
@@ -13,13 +13,13 @@ let data = require('./info.js');
 	const page = await browser.newPage();
 	await page.goto(data.info.website);
 
-	await page.type('#first_name', data.info.fname)
-	await page.type('#last_name', data.info.lname)
-	await page.type('#email', data.info.email)
-	await page.type('#street', data.info.address)
-	await page.type('#city', data.info.city)
-	await page.type('#state', data.info.state)
-	await page.type('#zip_code', data.info.zip)
+	await page.type('#ID', data.info.fname)
+	await page.type('#ID', data.info.lname)
+	await page.type('#ID', data.info.email)
+	await page.type('#ID', data.info.address)
+	await page.type('#ID', data.info.city)
+	await page.type('#ID', data.info.state)
+	await page.type('#ID', data.info.zip)
 
 	await page.$$eval('input[type="checkbox"]', checkboxes=> {
 		checkboxes.forEach(chkbox => chkbox.click())
@@ -27,14 +27,14 @@ let data = require('./info.js');
 
 	await Promise.all([
 		page.waitForNavigation(),
-		page.click('#send2')
+		page.click('#ID_OF_BUTTON')
 	]);
 
-	await page.screenshot({path: data.info.screenshot, fullPage: true});
+	await page.screenshot({path: data.info.screenshot, fullPage: true}); // Confirm sent
 
 	await browser.close();
 
-	await console.log(dt.toString() + data.info.fname + " Success");
+	await console.log(dt.toString() + " " + data.info.fname + " Success"); // Confirm in console, but verify with the screenshot
 
 })();
 
@@ -42,11 +42,11 @@ let data = require('./info.js');
 	var logger = fs.createWriteStream(data.info.log, {
 		flags: 'a' // append 
 	})
-	logger.write("\r\n"+ dt.toString() + data.info.fname + "  Success");
+	logger.write("\r\n"+ dt.toString() + " " + data.info.fname + " Success"); // Log your entries
 
 
 }
 
-setInterval(intervalFunc, 21720000); // 6 hours 2 minutes
+setInterval(intervalFunc, 21600000*4); // 24 Hours
 
 
