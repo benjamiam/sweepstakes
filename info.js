9de2d68df802faab6767831d187bca700b2fb0fc
benjamiam <bblackmon@gmail.com>
2020-04-15 14:38:02 -0500

    initial commit

---
+++ info.js
@@ -0,0 +1,15 @@
+let info = {
+	fname: "",
+	lname: "",
+	email: "",
+	address: "",
+	city: "",
+	state: "",
+	zip: "",
+
+	website: 'https://..',
+	screenshot: "/home/screenshot.png",	
+	log: "/home/log.txt", 
+}
+
+module.exports.info = info
